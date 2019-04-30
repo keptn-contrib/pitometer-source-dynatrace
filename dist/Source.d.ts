93edc5ad01f6ddcaeaaad50f37d46f93214d40b4
Daniel Khan <daniel@khan.io>
2019-04-30 13:33:04 +0200

    remove dist

--- dist/Source.d.ts
+++
@@ -1,7 +0,0 @@
-import * as pitometer from 'pitometer';
-import { IDynatraceOptions } from '@dynatrace/api-client';
-export declare class Source implements pitometer.ISource {
-    private dynatraceApi;
-    constructor(config: IDynatraceOptions);
-    fetch(query: any): Promise<number | boolean>;
-}
